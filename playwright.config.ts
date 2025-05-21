d41cbc7a448eed3ec2218d937d946b19483ec0eb
allancardoso100teddy <allan.messias@teddydigital.io>
2025-05-21 14:51:58 -0300

    ci: install only chromium with dependencies for GitHub actions is fixed now

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,14 +4,10 @@ export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    //headless: true,
-    // launchOptions: {
-    //   slowMo: 300,
-    // },
     video: "on",
     screenshot: "only-on-failure",
     trace: "on-first-retry",
