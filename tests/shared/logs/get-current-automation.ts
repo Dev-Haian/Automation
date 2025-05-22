c54bd4440c51f5699200cba53f809a866dac572b
allancardoso100teddy <allan.messias@teddydigital.io>
2025-05-22 09:56:24 -0300

    feat: 'logs' folder added with 'get-current-automation.ts' file to get log on any terminals.

---
+++ tests/shared/logs/get-current-automation.ts
@@ -0,0 +1,13 @@
+import fs from "node:fs";
+
+/**
+ * "getCurrentAutomation" É uma função utilitária para imprimir no terminal a automação que está sendo executada
+ * e a versão da mesma!
+ *
+ * **NOTA**: isso será refletido em qualquer terminal, seja local, em cloud e até mesmo nas actions do GitHub
+ */
+export function getCurrentAutomation(sut: string) {
+  const { version } = JSON.parse(fs.readFileSync("package.json", "utf-8"));
+
+  console.log(`Automação: ${sut} -> Versão: ${version}`);
+}
