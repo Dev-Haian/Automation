8f4b974e62ac9d49dc07b0c2cf79ff16f6af687c
allancardoso100teddy <allan.messias@teddydigital.io>
2025-07-08 10:48:24 -0300

    fix: ajustes rápidos no corpo e propriedades do e-mail.

--- tests/shared/types/mail/mail-props.ts
+++
@@ -1,9 +0,0 @@
-import type { Page } from "@playwright/test";
-import type { ErrorContentBodyProps } from "./error-content-body-props";
-
-export type EmailProps = {
-  page: Page;
-  subject: string;
-  body: ErrorContentBodyProps;
-  pathToAttachment?: string;
-};
--- tests/shared/utils/mail/format-mail-body.ts
+++ tests/shared/utils/mail/format-mail-body.ts
@@ -4,11 +4,11 @@ export function formatMailBody(body: ErrorContentBodyProps): string {
   return `
 		Olá, prezados(as).
 
-		Ocorreu um erro na jornada de [${body.sut}]. Segue o erro:
+		Ocorreu um erro na jornada de "${body.sut}". Segue o erro:
 
-		Endpoint: ${JSON.stringify(body.endpoint)}
+		Endpoint: ${body.endpoint}
 		Método Http: ${body.httpMethod}
-		Status: ${JSON.stringify(body.statusCode)}
+		Status: ${body.statusCode}
 		JSON: ${JSON.stringify(body.jsonBody)}
 
 		Segue anexo(s) do erro:
